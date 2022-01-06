6bfab53bf1006104589b40332c3bdb7d4398d325
rishbhh <40095968+rishbhh@users.noreply.github.com>
2022-01-06 19:08:31 +0530

    update

--- assets/resources/js/main.js
+++ assets/resources/js/main.js
@@ -1,79 +1,214 @@
-
-
 // Header Js
 $("#search-icon").click(function() {
     $(".nav").toggleClass("search");
     $(".nav").toggleClass("no-search");
     $(".search-input").toggleClass("search-active");
-  });
-  
-  $('.menu-toggle').click(function(){
-     $(".nav").toggleClass("mobile-nav");
-     $(this).toggleClass("is-active");
-  });
+});
 
-  
+$('.menu-toggle').click(function() {
+    $(".nav").toggleClass("mobile-nav");
+    $(this).toggleClass("is-active");
+});
+
+
 // CounterJs
 
-$.fn.jQuerySimpleCounter = function( options ) {
-   var settings = $.extend({
-       start:  0,
-       end:    100,
-       easing: 'swing',
-       duration: 400,
-       complete: ''
-   }, options );
+$.fn.jQuerySimpleCounter = function(options) {
+    var settings = $.extend({
+        start: 0,
+        end: 100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options);
 
-   var thisElement = $(this);
+    var thisElement = $(this);
 
-   $({count: settings.start}).animate({count: settings.end}, {
-     duration: settings.duration,
-     easing: settings.easing,
-     step: function() {
-        var mathCount = Math.ceil(this.count);
-        thisElement.text(mathCount);
-     },
-     complete: settings.complete
-  });
+    $({ count: settings.start }).animate({ count: settings.end }, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function() {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
 };
 
 
-$('#number1').jQuerySimpleCounter({end: 12,duration: 3000});
-$('#number2').jQuerySimpleCounter({end: 55,duration: 3000});
-$('#number3').jQuerySimpleCounter({end: 359,duration: 2000});
-$('#number4').jQuerySimpleCounter({end: 246,duration: 2500});
+$('#number1').jQuerySimpleCounter({ end: 12, duration: 3000 });
+$('#number2').jQuerySimpleCounter({ end: 55, duration: 3000 });
+$('#number3').jQuerySimpleCounter({ end: 359, duration: 2000 });
+$('#number4').jQuerySimpleCounter({ end: 246, duration: 2500 });
 
 
 // FAQ Accordian 
 const accordionItemHeaders = document.querySelectorAll(
-   ".accordion-item-header"
- );
- 
- accordionItemHeaders.forEach((accordionItemHeader) => {
-   accordionItemHeader.addEventListener("click", (event) => {
-     // Allow to collapse one item at a time
-     const currentlyActiveAccordionItemHeader = document.querySelector(
-       ".accordion-item-header.active"
-     );
-     if (
-       currentlyActiveAccordionItemHeader &&
-       currentlyActiveAccordionItemHeader !== accordionItemHeader
-     ) {
-       currentlyActiveAccordionItemHeader.classList.toggle("active");
-       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-     }
-     //
- 
-     accordionItemHeader.classList.toggle("active");
-     const accordionItemBody = accordionItemHeader.nextElementSibling;
-     if (accordionItemHeader.classList.contains("active")) {
-       accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-     } else {
-       accordionItemBody.style.maxHeight = 0;
-     }
-   });
-   accordionItemHeader.addEventListener("click", function () {
-     document.getElementById("test").scrollIntoView({ behavior: "smooth" });
-   });
- });
- 
+    ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+    accordionItemHeader.addEventListener("click", (event) => {
+        // Allow to collapse one item at a time
+        const currentlyActiveAccordionItemHeader = document.querySelector(
+            ".accordion-item-header.active"
+        );
+        if (
+            currentlyActiveAccordionItemHeader &&
+            currentlyActiveAccordionItemHeader !== accordionItemHeader
+        ) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+        //
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    });
+    accordionItemHeader.addEventListener("click", function() {
+        document.getElementById("test").scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+
+//  Testimonial JS 
+// vars
+'use strict'
+var testim = document.getElementById("testim"),
+    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+    touchStartPos,
+    touchEndPos,
+    touchPosDiff,
+    ignoreTouch = 30;;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length - 1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+
+    testim.addEventListener("touchstart", function(e) {
+        touchStartPos = e.changedTouches[0].clientX;
+    })
+
+    testim.addEventListener("touchend", function(e) {
+        touchEndPos = e.changedTouches[0].clientX;
+
+        touchPosDiff = touchStartPos - touchEndPos;
+
+        console.log(touchPosDiff);
+        console.log(touchStartPos);
+        console.log(touchEndPos);
+
+
+        if (touchPosDiff > 0 + ignoreTouch) {
+            testimLeftArrow.click();
+        } else if (touchPosDiff < 0 - ignoreTouch) {
+            testimRightArrow.click();
+        } else {
+            return;
+        }
+
+    })
+}
+
+// LogoSlider
+
+$(document).ready(function() {
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
+        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
