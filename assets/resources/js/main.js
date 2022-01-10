e8cf41302bbebe54ea42595d4b371e224fd79710
rishbhh <40095968+rishbhh@users.noreply.github.com>
2022-01-10 12:37:33 +0530

    update

--- assets/resources/js/main.js
+++ assets/resources/js/main.js
@@ -374,30 +374,30 @@ window.onload = function() {
 }
 
 // LogoSlider
-
+$('.customer-logos').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
+    nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
+    responsive: [{
+        breakpoint: 768,
+        settings: {
+            slidesToShow: 3
+        }
+    }, {
+        breakpoint: 520,
+        settings: {
+            slidesToShow: 2
+        }
+    }]
+});
 $(document).ready(function() {
-    $('.customer-logos').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500,
-        arrows: false,
-        dots: false,
-        pauseOnHover: false,
-        prevArrow: '<i class="slick-prev fas fa-angle-left"></i>',
-        nextArrow: '<i class="slick-next fas fa-angle-right"></i>',
-        responsive: [{
-            breakpoint: 768,
-            settings: {
-                slidesToShow: 3
-            }
-        }, {
-            breakpoint: 520,
-            settings: {
-                slidesToShow: 2
-            }
-        }]
-    });
+
 });
 
 
