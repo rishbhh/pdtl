a1b1c023ba78bacc0acdc0f0bae01e7eb7514fbe
rishbhh <40095968+rishbhh@users.noreply.github.com>
2022-01-11 20:36:36 +0530

    update

--- assets/resources/js/main.js
+++ assets/resources/js/main.js
@@ -263,6 +263,260 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
 });
 
 
+// Traning
+const slider = (function() {
+
+    //const
+    const slider = document.getElementById("slider");
+    const sliderContent = document.querySelector(".slider-content");
+    const sliderWrapper = document.querySelector(".slider-content-wrapper");
+    const elements = document.querySelectorAll(".slider-content__item");
+    const sliderContentControls = createHTMLElement("div", "slider-content__controls");
+    let dotsWrapper = null;
+    let prevButton = null;
+    let nextButton = null;
+    let autoButton = null;
+    let leftArrow = null;
+    let rightArrow = null;
+    let intervalId = null;
+
+    // data
+    const itemsInfo = {
+        offset: 0,
+        position: {
+            current: 0,
+            min: 0,
+            max: elements.length - 1
+        },
+        intervalSpeed: 1500,
+
+        update: function(value) {
+            this.position.current = value;
+            this.offset = -value;
+        },
+        reset: function() {
+            this.position.current = 0;
+            this.offset = 0;
+        }
+    };
+
+    const controlsInfo = {
+        buttonsEnabled: false,
+        dotsEnabled: false,
+        prevButtonDisabled: true,
+        nextButtonDisabled: false,
+        autoMode: false
+    };
+
+
+    function init(props) {
+
+        if (slider && sliderContent && sliderWrapper && elements) {
+
+            if (props) {
+                if (props.currentItem) {
+                    if (parseInt(props.currentItem) >= itemsInfo.position.min && parseInt(props.currentItem) <= itemsInfo.position.max) {
+                        itemsInfo.position.current = props.currentItem;
+                        itemsInfo.offset = -props.currentItem;
+                    }
+                }
+
+                if (props.intervalSpeed) itemsInfo.intervalSpeed = props.intervalSpeed;
+                if (props.buttons) controlsInfo.buttonsEnabled = true;
+                if (props.dots) controlsInfo.dotsEnabled = true;
+                if (props.autoMode) controlsInfo.autoMode = true;
+            }
+
+            _createControls(controlsInfo.dotsEnabled, controlsInfo.buttonsEnabled);
+            _render();
+            _updateControlsInfo();
+
+
+            if (controlsInfo.autoMode) {
+                _startAutoMode()
+            }
+        } else {
+            console.log("");
+        }
+    }
+
+
+    function _updateControlsInfo() {
+        const { current, min, max } = itemsInfo.position;
+        controlsInfo.prevButtonDisabled = current <= min;
+        controlsInfo.nextButtonDisabled = current >= max;
+    }
+
+
+    function _createControls(dots = false, buttons = false) {
+
+
+        sliderContent.append(sliderContentControls);
+
+
+        createArrows();
+        buttons ? createButtons() : null;
+        dots ? createDots() : null;
+
+
+        function createArrows() {
+            const dValueLeftArrow = "M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z";
+            const dValueRightArrow = "M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z";
+            const leftArrowSVG = createSVG(dValueLeftArrow);
+            const rightArrowSVG = createSVG(dValueRightArrow);
+
+            leftArrow = createHTMLElement("div", "prev-arrow");
+            leftArrow.append(leftArrowSVG);
+            leftArrow.addEventListener("click", () => updateSliderPosition(itemsInfo.position.current - 1))
+
+            rightArrow = createHTMLElement("div", "next-arrow");
+            rightArrow.append(rightArrowSVG);
+            rightArrow.addEventListener("click", () => updateSliderPosition(itemsInfo.position.current + 1))
+
+            sliderContentControls.append(leftArrow, rightArrow);
+
+            // SVG function
+            function createSVG(dValue, color = "currentColor") {
+                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                svg.setAttribute("viewBox", "0 0 256 512");
+                const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+                path.setAttribute("fill", color);
+                path.setAttribute("d", dValue);
+                svg.appendChild(path);
+                return svg;
+            }
+        }
+
+        // Dots function
+        function createDots() {
+            dotsWrapper = createHTMLElement("div", "dots");
+            for (let i = 0; i < itemsInfo.position.max + 1; i++) {
+                const dot = document.createElement("div");
+                dot.className = "dot";
+                dot.addEventListener("click", function() {
+                    updateSliderPosition(i);
+                })
+                dotsWrapper.append(dot);
+            }
+            sliderContentControls.append(dotsWrapper);
+        }
+
+        // Buttons function
+        function createButtons() {
+            const controlsWrapper = createHTMLElement("div", "slider-controls");
+            prevButton = createHTMLElement("button", "prev-control", "Prev");
+            prevButton.addEventListener("click", () => updateSliderPosition(itemsInfo.position.current - 1))
+
+            autoButton = createHTMLElement("button", "auto-control", "Auto");
+            autoButton.addEventListener("click", () => intervalId ? _stopAutoMode() : _startAutoMode())
+
+            nextButton = createHTMLElement("button", "next-control", "Next");
+            nextButton.addEventListener("click", () => updateSliderPosition(itemsInfo.position.current + 1))
+
+            controlsWrapper.append(prevButton, autoButton, nextButton);
+            slider.append(controlsWrapper);
+        }
+    }
+
+
+    function _stopAutoMode() {
+        clearInterval(intervalId)
+        intervalId = null
+        if (autoButton) autoButton.textContent = 'Auto'
+    }
+
+
+    function _startAutoMode() {
+        controlsInfo.autoMode = true
+        if (autoButton) autoButton.textContent = 'Stop'
+        intervalId = setInterval(function() {
+            if (itemsInfo.position.current < itemsInfo.position.max) {
+                itemsInfo.update(itemsInfo.position.current + 1);
+            } else {
+                itemsInfo.reset();
+            }
+            _slideItem();
+        }, itemsInfo.intervalSpeed)
+    }
+
+
+    function setClass(options) {
+        if (options) {
+            options.forEach(({ element, className, disabled }) => {
+                if (element) {
+                    disabled ? element.classList.add(className) : element.classList.remove(className)
+                } else {
+                    console.log("Error: function setClass(): element = ", element);
+                }
+            })
+        }
+    }
+
+
+    function updateSliderPosition(value) {
+        itemsInfo.update(value);
+        controlsInfo.autoMode = false
+        _slideItem();
+    }
+
+    function _render() {
+        const { prevButtonDisabled, nextButtonDisabled } = controlsInfo;
+        let controlsArray = [
+            { element: leftArrow, className: "d-none", disabled: prevButtonDisabled },
+            { element: rightArrow, className: "d-none", disabled: nextButtonDisabled }
+        ];
+        if (controlsInfo.buttonsEnabled) {
+            controlsArray = [
+                ...controlsArray,
+                { element: prevButton, className: "disabled", disabled: prevButtonDisabled },
+                { element: nextButton, className: "disabled", disabled: nextButtonDisabled }
+            ];
+        }
+
+
+        setClass(controlsArray);
+
+
+        sliderWrapper.style.transform = `translateX(${itemsInfo.offset * 100}%)`;
+
+
+        if (controlsInfo.dotsEnabled) {
+            if (document.querySelector(".dot--active")) {
+                document.querySelector(".dot--active").classList.remove("dot--active");
+            }
+            dotsWrapper.children[itemsInfo.position.current].classList.add("dot--active");
+        }
+    }
+
+
+    function _slideItem() {
+        if (!controlsInfo.autoMode && intervalId) {
+            _stopAutoMode()
+        }
+        _updateControlsInfo();
+        _render();
+    }
+
+
+    function createHTMLElement(tagName = "div", className, innerHTML) {
+        const element = document.createElement(tagName);
+        element.className = className || null;
+        element.innerHTML = innerHTML || null;
+        return element;
+    }
+
+
+    return { init };
+}())
+
+slider.init({
+    // intervalSpeed: 500,
+    // currentItem: 2,
+    buttons: false,
+    dots: false,
+    autoMode: false,
+    loop: true
+});
 
 
 // LogoSlider
@@ -351,3 +605,34 @@ $(window).scroll(function() {
         $('nav div').removeClass('visible-title');
     }
 });
+
+
+// tabs 
+var links = document.querySelectorAll('.tabs ul li a');
+var content = document.querySelectorAll('div.content');
+var border = document.querySelector('.tabs span');
+var lis = document.querySelectorAll('.tabs ul li');
+
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        var activLinks = document.querySelector('.tabs ul li a.activ');
+        var activContent = document.querySelector('.tabs-div>div.activ');
+
+        activLinks.classList.remove('activ');
+        activContent.classList.remove('activ');
+
+        this.classList.add('activ');
+        var attr = this.getAttribute('href');
+
+        var activ = document.querySelector(attr);
+
+        activ.classList.add('activ');
+
+        var lisLength = lis.length;
+        var lisWidth = 100 / lisLength;
+        var position = i * lisWidth;
+        border.style.left = position + '%';
+
+    });
+}
