c29f6110e771fa056de322cf9885d82792309e8d
rishbhh <40095968+rishbhh@users.noreply.github.com>
2022-01-27 18:01:08 +0530

    updat

--- assets/resources/js/main.js
+++ assets/resources/js/main.js
@@ -244,16 +244,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-$('.carousel-main').owlCarousel({
-    items: 1,
-    loop: true,
-    autoplay: true,
-    autoplayTimeout: 2000,
-    margin: 10,
-    nav: true,
-    dots: false,
-    navText: ['<span class="fas fa-chevron-left fa-2x"></span>', '<span class="fas fa-chevron-right fa-2x"></span>'],
-})
 
 
 // Traning
